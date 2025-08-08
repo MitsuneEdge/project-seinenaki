c0f6a24676e50caab9b395319199d2123e21e187
Matsushita Rintawa <1961009064@qq.com>
2025-08-08 19:39:52 +0800

    2025.8.8修改

--- src/script/indexOverlayWindow.js
+++ src/script/indexOverlayWindow.js
@@ -17,12 +17,6 @@ if (typeof window !== 'undefined') {
     trigger.addEventListener('click', (e) => {
         // 阻止事件冒泡（避免立即触发关闭）
         e.stopPropagation();
-        const rect = trigger.getBoundingClientRect();
-        // 获取视口高度并计算90vh的像素值
-        const modalHeight = window.innerHeight * 0.9; // 90vh
-
-        // 设置模态窗口初始位置（与触发元素水平对齐）
-        content.style.top = `${rect.top - (modalHeight / 2) + rect.height}px`;
         overlay.classList.replace('window-hidden', 'window-visible');
         });
 
@@ -32,7 +26,7 @@ if (typeof window !== 'undefined') {
         });
 
         // 阻止内容区域点击关闭
-        Content.addEventListener('click', (e) => {
+        content.addEventListener('click', (e) => {
             e.stopPropagation();
         });
     });
