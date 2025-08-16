551632d8be11028e022c2d1bddad79afb61c4e2b
Matsushita Rintawa <1961009064@qq.com>
2025-08-16 17:50:07 +0800

    2025.8.16修改

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Rinkawa-Main 洛高集团正在测试";
-export const SITE_DESCRIPTION = "你他妈的给我滚出去!";
+export const SITE_TITLE = "Resonote Studio";
+export const SITE_DESCRIPTION = "Resonote Studio";
